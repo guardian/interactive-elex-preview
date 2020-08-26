c70ec81ea3fe2282bb55ddc3ca418ed980c44423
Helena Robertson <h.robertson@icloud.com>
2020-08-26 17:45:15 +0100

    first buttons

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -17,24 +17,24 @@ async function loadGroupData() {
     return groups
 }
 
+
+
 loadGroupData().then(groups => {
     const groupsInUse = groups.filter(group => group.states)
     const boxes = createBoxes(groupsInUse)
-    console.log(groupsInUse)
     const solids = groups.filter(group => group.solid)
-    const solidData = {
-        trump: solids[0].ev_count,
-        biden: solids[1].ev_count
-    }
-    // createInitialBars(solidData)
 
+    createInitialGraphics(solids)
     // boxes.onChange(data => {
     //     bars.update(data);
-    // }
+    // })
 
-    // console.log(solidData)
+    console.log(groupsInUse)
+
 })
 
+const bidenCol = '#25428f'
+const trumpCol = '#cc0a11'
 
 function createBoxes(groups) {
     const groupsDiv = d3.select('.state-groups')
@@ -66,38 +66,140 @@ function createBoxes(groups) {
         .html(d => d.storyText)
         .classed("state-group__context", true)
 
-    const buttonDiv = groupDivs
+    const setToggle = (groupDiv, d) => {
+        const options = groupDiv.selectAll('.state-group-toggle__option')
+        const inner = groupDiv.select('.state-group-toggle__inner')
+
+        const perc = d === 'biden' ? 0 : (d === 'unselected' ? 33.3333 : 66.6667)
+        const backCol = d === 'biden' ? bidenCol : (d === 'unselected' ? "#dcdcdc" : trumpCol)
+
+        inner.style('left', perc + '%')
+        inner.style('background-color', backCol)
+
+        options.classed('state-group-toggle__option--selected', d2 => {
+            return d2 === d
+        })
+    }
+
+    const toggleDiv = groupDivs
         .append('div')
-        .classed("state-group__buttondiv", true)
+        .classed("state-group__toggle", true)
+
+    const inner = toggleDiv
+        .append('div')
+        .classed('state-group-toggle__inner', true)
+
+    const options = toggleDiv
+        .selectAll('blah')
+        .data(['biden', 'unselected', 'trump'])
+        .enter()
+        .append('div')
+        .attr('class', d => `state-group-toggle__option state-group-toggle__option--${d.toLowerCase()}`)
+        .text(d => d.slice(0, 1).toUpperCase() + d.slice(1))
+
+    groupDivs.each(function (d, i) {
+        const groupDiv = d3.select(this)
+        setToggle(groupDiv, d.candidate_select)
+    })
+
 }
 
 
-// function createInitialBars(solidData) {
+function createInitialGraphics(solids) {
 
-//     // Bar setup
-//     const x = d3.scaleLinear().domain([0, 538]).range([0, 100])
+    // Bar setup
+    const x = d3.scaleLinear().domain([0, 538]).range([0, 100])
 
-//     const y = d3.scaleLinear()
-//         .domain([0, 1])
-//         .range([0, 100])
+    const y = d3.scaleLinear()
+        .domain([0, 1])
+        .range([0, 100])
 
-//     // 270 majority finish line
-//     const finish = d3.select('.elex-votes')
-//         .append('div')
-//         .attr("class", 'elex-votes-finishline')
-//         .style('left', x(270) + '%');
+    // 270 majority finish line
+    const finish = d3.select('.elex-votes')
+        .append('div')
+        .attr("class", 'elex-votes-finishline')
+        .style('left', x(270) + '%');
 
-//     finish
-//         .append('div')
-//         .attr("class", "elex-votes-finish-label")
-//         .text("270 to win");
+    finish
+        .append('div')
+        .attr("class", "elex-votes-finish-label")
+        .text("270 to win");
 
-//     // Fill solid
-//     const trumpBar = d3.select('.bar_container__trump')
-//     const bidenBar = d3.select('.bar_container__biden')
-//         .style("width", x(solidData.biden) + '%')
-//     console.log(x(solidData.biden) + '%')
-// }
+    const solidData = {
+        trump: solids[0].ev_count,
+        biden: solids[1].ev_count
+    }
+
+    const trumpSolidStates = solids[0].solid_states.split(", ")
+
+    const bidenSolidStates = solids[1].solid_states.split(", ")
+
+    console.log(trumpSolidStates)
+    console.log(bidenSolidStates)
+
+    // Fill solid
+    const trumpBar = d3.select('.bar-container__trump')
+        .style("width", x(solidData.trump) + '%')
+        .style("left", x(538) - x(solidData.trump) + '%')
+    const bidenBar = d3.select('.bar-container__biden')
+        .style("width", x(solidData.biden) + '%')
+    console.log(x(538) - x(solidData.trump) + '%')
+
+
+    // Map setup
+    const statesFc = topojson.feature(statesTopo, statesTopo.objects.states)
+
+    // 'fc' is short for 'FeatureCollection', you can log it to look at the structure
+    console.log(statesFc)
+
+    const draw = () => {
+
+        // $ is shorthand for document.querySelector
+        // selects the SVG element
+        const svgEl = $('.elex-map')
+
+        // get the SVG's width as set in CSS
+        const width = svgEl.getBoundingClientRect().width
+        const height = width * 0.66
+
+        const svg = d3.select(svgEl)
+            .attr('width', width)
+            .attr('height', height)
+
+        // set up a map projection that fits our GeoJSON into the SVG
+
+        const proj = d3.geoAlbersUsa()
+            .fitExtent([
+                [-69, 0],
+                [width, height]
+            ], statesFc)
+
+        const path = d3.geoPath().projection(proj)
+
+        // Draw states
+
+        const stateShapes = svg
+            .selectAll('blah')
+            // do something for each feature ( = area ) in the GeoJSON
+            .data(statesFc.features)
+            .enter()
+            .append('path')
+            .style("fill", function (d) {
+                if (trumpSolidStates.includes(d.properties.name) == true) {
+                    return trumpCol
+                } else if (bidenSolidStates.includes(d.properties.name) == true) {
+                    return bidenCol
+                } else {
+                    return "#f6f6f6"
+                }
+            })
+            .attr('d', path)
+            .attr('class', 'elex-state')
+
+    }
+    // call the draw function
+    draw()
+}
 
 
 // Filled status headline
@@ -107,50 +209,3 @@ function createBoxes(groups) {
 //     .style('color', votesFor > votesAgainst ? statusHex.succeed : statusHex.fail)
 //     .transition()
 //     .text(votesFor > votesAgainst ? 'pass' : 'reject')
-
-
-
-// Map setup
-const statesFc = topojson.feature(statesTopo, statesTopo.objects.states)
-
-// 'fc' is short for 'FeatureCollection', you can log it to look at the structure
-console.log(statesFc)
-
-const draw = () => {
-
-    // $ is shorthand for document.querySelector
-    // selects the SVG element
-    const svgEl = $('.elex-map')
-
-    // get the SVG's width as set in CSS
-    const width = svgEl.getBoundingClientRect().width
-    const height = width * 0.66
-
-    const svg = d3.select(svgEl)
-        .attr('width', width)
-        .attr('height', height)
-
-    // set up a map projection that fits our GeoJSON into the SVG
-
-    const proj = d3.geoAlbersUsa()
-        .fitExtent([
-            [-69, 0],
-            [width, height]
-        ], statesFc)
-
-    const path = d3.geoPath().projection(proj)
-
-    // Draw states
-
-    const stateShapes = svg
-        .selectAll('blah')
-        // do something for each feature ( = area ) in the GeoJSON
-        .data(statesFc.features)
-        .enter()
-        .append('path')
-        .attr('d', path)
-        .attr('class', 'elex-state')
-
-}
-// call the draw function
-draw()
