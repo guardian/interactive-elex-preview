6aa53c64f01b2b738a8cd45c000b769d4d9e1650
tinius <nkommenda@hotmail.com>
2020-09-08 10:52:34 +0100

    some fixes to event handlers

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -71,8 +71,8 @@ loadData().then(groups => {
     console.log(cards)
     console.log(barAndMap)
 
-    cards.onChange(data => {
-        barAndMap.update(data)
+    cards.onChange((data, oldData) => {
+        barAndMap.update(data, oldData)
     })
 
 })
@@ -280,6 +280,7 @@ class stateCards {
 
 
     onChange(callback) {
+
         const that = this
 
         this.stateDivs.each(function (td) {
@@ -289,15 +290,18 @@ class stateCards {
             options.on('click', function (d, i) {
                 const candidate_select = d.toLowerCase();
 
+            
+            const oldStatesInUse = that.statesInUse
+
                 that.statesInUse = that.statesInUse.map(t => {
-                    return t.id === td.id ? Object.assign({}, t, {
+                    return t.state === td.state ? Object.assign({}, t, {
                             candidate_select
                         }) :
                         t
                 })
 
                 setButtons(stateDiv, d)
-                callback(that.stateInUse)
+                callback(that.statesInUse, oldStatesInUse)
             })
 
         })
@@ -433,9 +437,20 @@ class initialGraphics {
         }
     }
 
-    update(data) {
-        const newTrumpTotal = this.barTotalData.trump
-        const newBidenTotal = this.barTotalData.biden
+    update(data, oldData) {
+
+        const sum = (a, b) => a + b
+
+        console.log(data, oldData)
+
+        const newTrumpTotal = data.filter( d => d.candidate_select === "trump")
+            .map( d => Number(d.ecvs) ).reduce(sum, 0)
+
+        const newBidenTotal = data.filter( d => d.candidate_select === "biden")
+            .map( d => Number(d.ecvs) ).reduce(sum, 0)
+    
+        // const newTrumpTotal = Number(this.barTotalData.trump)
+        // const newBidenTotal = Number(this.barTotalData.biden)
 
         updateElexBarGraphic(newBidenTotal, newTrumpTotal, 0, 0)
     }
@@ -580,6 +595,8 @@ function changePortrait(candidate, mood) {
 // ANIMATE VOTES TOTAL
 
 function animateTotal(candidate, newTotal, currentTotal) {
+
+    console.log(candidate, newTotal, currentTotal)
 
     const total = d3.select('.' + candidate + "-title-count");
     let flashWin = false;
